b14c306e4931c2e53db97de0bd3f06027e8b0073
diego <deigo555g@gmail.com>
2025-12-18 20:11:39 -0300

    feat: criando funções para para, iniciar e remover um container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import docker 
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 from tela import Ui_MainWindow
 
 class MainWindow(QMainWindow):
@@ -8,22 +9,61 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        # Conectando os botões
+        self.client = docker.from_env()
+
+        self.listar_containers()
+        
         self.ui.startButton.clicked.connect(self.start_container)
         self.ui.stopButton.clicked.connect(self.stop_container)
         self.ui.removeButton.clicked.connect(self.remove_container)
 
+    def listar_containers(self):
+        containers = self.client.containers.list(all=True)
+        self.ui.tableContainers.setRowCount(len(containers))
+
+        for row, c in enumerate(containers):
+            short_id = c.id[:12]
+            name = c.name
+            image = c.image.tags[0] if c.image.tags else "none"
+
+            status = c.status
+            
+            self.ui.tableContainers.setItem(row, 0, QTableWidgetItem(short_id))
+            self.ui.tableContainers.setItem(row, 1, QTableWidgetItem(name))
+            self.ui.tableContainers.setItem(row, 2, QTableWidgetItem(image))
+            self.ui.tableContainers.setItem(row, 3, QTableWidgetItem(status))
+
+
+    def container_selecionado(self):
+        row = self.ui.tableContainers.currentRow()
+        if row == -1:
+            return None
+        return self.ui.tableContainers.item(row, 1).text()
+
+
     def start_container(self):
-        print("START clicado")
+        nome = self.container_selecionado()
+        if nome:
+            self.client.containers.get(nome).start()
+            self.listar_containers()
+
 
     def stop_container(self):
-        print("STOP clicado")
+        nome = self.container_selecionado()
+        if nome:
+            self.client.containers.get(nome).stop()
+            self.listar_containers()
+
 
     def remove_container(self):
-        print("REMOVE clicado")
+        nome = self.container_selecionado()
+        if nome:
+            self.client.containers.get(nome).remove(force=True)
+            self.listar_containers()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = MainWindow()   
     window.show()
     sys.exit(app.exec_())
